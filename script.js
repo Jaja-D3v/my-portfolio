e3371d66a106063e1f4a6ceda55525a14314ee5e
Jared | Dev <jaredabrera44@gmail.com>
2026-09-11 09:23:23 +0800

    added animation

--- script.js
+++ script.js
@@ -22,4 +22,20 @@ navLinks.forEach(link => {
     });
 });
 
-// for cv 
+// this is for animation
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+reveals.forEach((element) => {
+    observer.observe(element);
+});
